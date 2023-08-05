08bb665ba66d1d3c6697d6e20171fd9497f2df86
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-05 22:00:43 +0530

    Reusable widgets and constructor funtions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,18 +4,19 @@ import 'package:f_app_1/styled_text.dart';
 const  startAlignment=Alignment.topLeft;
 const endAlignment=Alignment.topRight;
 
+
 class GradientContainer extends StatelessWidget{
   // GradientContainer({key}):super(key:key); //the constructor needs a named argument 'key' , which is passed to super's key argument
   // alternative to previous line :      ---- named arguments are to be obtained inside '{}'
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2 ,{super.key});
+  final Color color1;
+  final Color color2;
   @override
   Widget build(context){
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 51, 19, 107) ,
-             Color.fromARGB(255, 78, 10, 194)
-             ],
+            colors:[color1,color2],
              begin: startAlignment,
              end:endAlignment,
           ),
@@ -26,3 +27,25 @@ class GradientContainer extends StatelessWidget{
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget{
+//   // GradientContainer({key}):super(key:key); //the constructor needs a named argument 'key' , which is passed to super's key argument
+//   // alternative to previous line :      ---- named arguments are to be obtained inside '{}'
+//   const GradientContainer({super.key , required this.colors});
+//   final List<Color> colors;
+//   @override
+//   Widget build(context){
+//     return Container(
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors:colors,
+//              begin: startAlignment,
+//              end:endAlignment,
+//           ),
+//         ),
+//         child: const Center(
+//           child:StyledText('Hi')
+//       ),
+//     );
+//   }
+// }
