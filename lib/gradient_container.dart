56c545a5fae5fc77dc3ab1b390cd1858cb58e5c0
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-06 06:15:46 +0530

    Adding dice images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:f_app_1/styled_text.dart';
+// import 'package:f_app_1/styled_text.dart';
 
 const  startAlignment=Alignment.topLeft;
 const endAlignment=Alignment.topRight;
@@ -8,9 +8,15 @@ const endAlignment=Alignment.topRight;
 class GradientContainer extends StatelessWidget{
   // GradientContainer({key}):super(key:key); //the constructor needs a named argument 'key' , which is passed to super's key argument
   // alternative to previous line :      ---- named arguments are to be obtained inside '{}'
+
   const GradientContainer(this.color1, this.color2 ,{super.key});
   final Color color1;
   final Color color2;
+
+  void rollDice(){
+    
+  }
+
   @override
   Widget build(context){
     return Container(
@@ -21,8 +27,14 @@ class GradientContainer extends StatelessWidget{
              end:endAlignment,
           ),
         ),
-        child: const Center(
-          child:StyledText('Hi')
+        child: Center(
+          child:Column(
+            children:[
+              Image.asset("assets/images/dice-1.png",
+              width: 200,
+              ),
+              TextButton(onPressed: rollDice, child: Text('Roll It')),
+          ],) 
       ),
     );
   }
